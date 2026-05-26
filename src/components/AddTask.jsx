12da340b1337dcb69e0489ec2e4f44401258ffd3
Clara Bonan <clarabonan01@gmail.com>
2026-05-26 16:21:29 -0300

    commit incial: projeto completo mas sem alteracoes

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import  Input  from "./Input";
+
+function AddTask({ onAddTaskSubmit }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  return (
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+      <Input
+        type="text"
+        placeholder="Digite o Título da Tarefa"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <Input
+        type="text"
+        placeholder="Digite a Descrição da Tarefa"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <button
+        className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
+        onClick={() => {
+          if (!title.trim() || !description.trim()) {
+            alert("Preencha o título e a descrição da tarefa");
+            return;
+          }
+          onAddTaskSubmit(title, description);
+          setTitle("");
+          setDescription("");
+        }}
+      >
+        Adicionar
+      </button>
+    </div>
+  );
+}
+
+export default AddTask;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,46 @@
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import Button from "./Button";
+
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
+  const navigate = useNavigate();
+
+  function onSeeDatailsClick(task) {
+    const queryParams = new URLSearchParams();
+    queryParams.set("title", task.title);
+    queryParams.set("description", task.description);
+    navigate(`/task/?${queryParams.toString()}`);
+  }
+
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => onTaskClick(task.id)}
+            // Ajustado para evitar renderizar a palavra "false" na classe
+            className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
+              task.isCompleted ? "line-through" : ""
+            }`}
+          >
+            {task.title}
+          </button>
+
+          <Button
+            onClick={() => onSeeDatailsClick(task)}
+          >
+            <ChevronRightIcon />
+          </Button>
+
+          <Button
+            onClick={() => onDeleteTaskClick(task.id)}
+          >
+            <TrashIcon />
+          </Button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Tasks;
